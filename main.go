8d57ac8da47c29f608efc7a7ef67bf7524c71d0a
Andrew Howden <hello@andrewhowden.com>
2020-03-24 18:49:40 +0100

    Added additional logging, quit capability

--- main.go
+++ main.go
@@ -4,23 +4,28 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"syscall"
 )
+
+func handle(sig os.Signal) {
+	switch sig {
+	case syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT:
+		fmt.Printf("Received %s. Exiting\n", sig)
+		os.Exit(0)
+	default:
+		fmt.Printf("Received %s\n", sig)
+	}
+}
 
 func main() {
 
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
+	defer close(done)
 
 	signal.Notify(sigs)
 
-	go func() {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <- true
-	}()
-
-	fmt.Println("awaiting signal")
-	<-done
-	fmt.Println("exiting")
+	for sig := range sigs {
+		handle(sig)
+	}
 }
